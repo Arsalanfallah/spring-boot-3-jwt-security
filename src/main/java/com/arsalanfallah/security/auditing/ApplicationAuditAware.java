36fe82b4ff1b476fa5dc6c9493df61db681f7ec0
a_fallah <Arsalan_fallah@yahoo.com>
2024-11-06 23:50:01 +0330

    add

--- src/main/java/com/arsalanfallah/security/auditing/ApplicationAuditAware.java
+++ src/main/java/com/arsalanfallah/security/auditing/ApplicationAuditAware.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 public class ApplicationAuditAware implements AuditorAware<Integer> {
     @Override
     public Optional<Integer> getCurrentAuditor() {
+
         Authentication authentication =
                 SecurityContextHolder
                         .getContext()
